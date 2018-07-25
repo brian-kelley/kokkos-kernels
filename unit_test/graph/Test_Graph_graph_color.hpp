c598b3f7fd0315eb64fdb7758de4f5933662b493
Brian Kelley <bmkelle@sandia.gov>
2018-07-25 09:56:38 -0600

    Added test and graph file to replicate graph coloring crash

--- unit_test/graph/Test_Graph_graph_color.hpp
+++ unit_test/graph/Test_Graph_graph_color.hpp
@@ -43,6 +43,8 @@
 
 #include <gtest/gtest.h>
 #include <Kokkos_Core.hpp>
+#include <set>
+#include <utility>
 
 #include "KokkosGraph_graph_color.hpp"
 #include "KokkosSparse_CrsMatrix.hpp"
@@ -176,10 +178,80 @@ void test_coloring(lno_t numRows,size_type nnz, lno_t bandwidth, lno_t row_size_
 
 }
 
+
+template <typename scalar_t, typename lno_t, typename offset_t, typename device>
+void test_gc_crash(std::string graphPath)
+{
+  typedef typename KokkosSparse::CrsMatrix<scalar_t, lno_t, device, void, offset_t> crsMat_t;
+  typedef typename crsMat_t::StaticCrsGraphType graph_t;
+  typedef typename crsMat_t::values_type::non_const_type scalar_view_t;
+  typedef typename graph_t::row_map_type::non_const_type lno_view_t;
+  typedef typename graph_t::entries_type::non_const_type lno_nnz_view_t;
+  typedef typename lno_view_t::size_type size_type;
+  lno_t nrows;
+  offset_t numEntries; 
+  offset_t* xadjRaw;
+  lno_t* adjRaw;
+  scalar_t* scalarRaw;
+  KokkosKernels::Impl::read_graph_crs<lno_t, offset_t, scalar_t>
+    (&nrows, &numEntries, &xadjRaw, &adjRaw, &scalarRaw, graphPath.c_str());
+  lno_view_t xadj("xadj", nrows + 1);
+  for(size_type i = 0; i < nrows + 1; i++)
+    xadj(i) = xadjRaw[i];
+  lno_view_t adj("adj", numEntries);
+  for(size_type i = 0; i < numEntries; i++)
+    adj(i) = adjRaw[i];
+  std::cout << "Read in CRS graph with " << nrows << " rows and " << numEntries << " entries.\n";
+  std::set<std::pair<lno_t, lno_t>> entrySet;
+  //Test that graph is valid (columns in 0...nrows) and symmetric
+  for(lno_t i = 0; i < nrows; i++)
+  {
+    for(offset_t j = xadj(i); j < xadj(i + 1); j++)
+    {
+      auto col = adj(j);
+      if(col >= nrows)
+      {
+        std::cout << "Entry in row " << i << " is invalid (column " << col << ", but graph has " << nrows << " columns)\n";
+        return;
+      }
+      if(i != col)
+        entrySet.emplace(i, col);
+    }
+  }
+  for(lno_t i = 0; i < nrows; i++)
+  {
+    for(offset_t j = xadj(i); j < xadj(i + 1); j++)
+    {
+      auto col = adj(j);
+      //make sure transposed entry also exists
+      std::pair<lno_t, lno_t> search(col, i);
+      if(i != col && entrySet.find(search) == entrySet.end())
+      {
+        std::cout << "Entry at " << i << ", " << col << " breaks symmetry.\n";
+        return;
+      }
+    }
+  }
+  typedef KokkosKernelsHandle
+    <offset_t, lno_t, scalar_t,
+    typename device::execution_space, typename device::memory_space,typename device::memory_space> KernelHandle;
+  KernelHandle kh;
+  kh.create_graph_coloring_handle();
+  std::cout << "Calling graph color kernel on cluster test graph..." << std::endl;
+  KokkosGraph::Experimental::graph_color_symbolic(&kh, nrows, nrows, xadj, adj, false); 
+  auto coloringHandle = kh.get_graph_coloring_handle();
+  auto clusterColors = coloringHandle->get_vertex_colors();
+  auto numClusterColors = coloringHandle->get_num_colors();
+  std::cout << "Success, used " << numClusterColors << " colors." << std::endl;
+}
+
 #define EXECUTE_TEST(SCALAR, ORDINAL, OFFSET, DEVICE) \
 TEST_F( TestCategory, graph ## _ ## graph_color ## _ ## SCALAR ## _ ## ORDINAL ## _ ## OFFSET ## _ ## DEVICE ) { \
   test_coloring<SCALAR,ORDINAL,OFFSET,DEVICE>(50000, 50000 * 30, 200, 10); \
   test_coloring<SCALAR,ORDINAL,OFFSET,DEVICE>(50000, 50000 * 30, 100, 10); \
+} \
+TEST_F( TestCategory, graph ## _ ## BMK_replicate_color_crash ## _ ## SCALAR ## _ ## ORDINAL ## _ ## OFFSET ## _ ## DEVICE ) { \
+  test_gc_crash<SCALAR,ORDINAL,OFFSET,DEVICE>("debugGraph.txt"); \
 }
 
 #if (defined (KOKKOSKERNELS_INST_ORDINAL_INT) \
