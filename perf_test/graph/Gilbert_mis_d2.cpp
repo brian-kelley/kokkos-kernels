3b9b4e9119d0dc96cbbee6b741eabc3f08d8a52a
Brian Kelley <bmkelle@sandia.gov>
2020-09-01 18:59:41 -0600

    Fixed driver for Mike Gilbert's implementation

--- perf_test/graph/Gilbert_mis_d2.cpp
+++ perf_test/graph/Gilbert_mis_d2.cpp
@@ -15,21 +15,28 @@
 #include <sys/time.h>
 
 #include <Kokkos_Core.hpp>
+#include <Kokkos_Random.hpp>
 
 #include "KokkosKernels_Utils.hpp"
 #include "KokkosSparse_CrsMatrix.hpp"
 #include "KokkosKernels_default_types.hpp"
 
+template<typename matrix_type>
 Kokkos::View<int*> mis_2(const matrix_type& g)
 {
+    using sgp_vid_t = typename matrix_type::ordinal_type;
+    using sgp_eid_t = typename matrix_type::non_const_size_type;
+    using pool_t = Kokkos::Random_XorShift64_Pool<typename matrix_type::execution_space>;
+    using gen_t = typename pool_t::generator_type;
+    using vtx_view_t = typename matrix_type::index_type::non_const_type;
+
     sgp_vid_t n = g.numRows();
     Kokkos::View<int*> state("is membership", n);
     sgp_vid_t unassigned_total = n;
     Kokkos::View<uint64_t*> randoms("randomized", n);
     pool_t rand_pool(std::time(nullptr));
-    Kokkos::parallel_for("create random entries", n, KOKKOS_LAMBDA(sgp_vid_t i){
-        gen_t generator = rand_pool.get_state();
-        randoms(i) = generator.urand64();
+    Kokkos::parallel_for("create random entries", n,
+        KOKKOS_LAMBDA(sgp_vid_t i){ gen_t generator = rand_pool.get_state(); randoms(i) = generator.urand64();
         rand_pool.free_state(generator);
     });
     while (unassigned_total > 0)
@@ -48,7 +55,8 @@ Kokkos::View<int*> mis_2(const matrix_type& g)
                 Kokkos::parallel_for(n, KOKKOS_LAMBDA(const sgp_vid_t i){
                 int max_state = tuple_state(i);
                 uint64_t max_rand = tuple_rand(i);
-                sgp_vid_t max_idx = tuple_idx(i);                for (sgp_eid_t j = g.graph.row_map(i); j < g.graph.row_map(i + 1); j++) {
+                sgp_vid_t max_idx = tuple_idx(i);
+                for (sgp_eid_t j = g.graph.row_map(i); j < g.graph.row_map(i + 1); j++) {
                     sgp_vid_t v = g.graph.entries(j);
                     bool is_max = false;
                     if (tuple_state(v) > max_state) {
@@ -98,61 +106,38 @@ Kokkos::View<int*> mis_2(const matrix_type& g)
 
 int main(int argc, char *argv[])
 {
-    MIS2Parameters params;
-
-    if(parse_inputs(params, argc, argv))
-    {
-        return 1;
-    }
-
-    if(params.mtx_file == NULL)
-    {
-        std::cerr << "Provide a matrix file" << std::endl;
-        return 0;
-    }
+    using device_t = Kokkos::DefaultExecutionSpace;
+    using size_type = default_size_type;
+    using lno_t = default_lno_t;
+    using exec_space = typename device_t::execution_space;
+    using mem_space = typename device_t::memory_space;
+    using crsMat_t = typename KokkosSparse::CrsMatrix<default_scalar, default_lno_t, device_t, void, default_size_type>;
+    using lno_view_t = typename crsMat_t::index_type::non_const_type;
 
     Kokkos::initialize();
-
-    bool run = false;
-
-    #if defined(KOKKOS_ENABLE_OPENMP)
-    if(params.use_openmp)
     {
-      run_mis2<Kokkos::OpenMP>(params);
-      run = true;
+      crsMat_t A = KokkosKernels::Impl::read_kokkos_crst_matrix<crsMat_t>(argv[1]);
+      //Compute MIS-2
+      Kokkos::Timer timer;
+      auto mis = mis_2(A);
+      double t = timer.seconds();
+      auto misHost = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), mis);
+      std::vector<int> misList;
+      FILE* out = fopen("mis2.txt", "w");
+      int misSize = 0;
+      for(int i = 0; i < A.numRows(); i++)
+      {
+        if(misHost(i) == 1)
+        {
+          fprintf(out, "%d ", i);
+          misSize++;
+        }
+      }
+      fclose(out);
+      std::cout << "Computed MIS in " << t << " s.\n";
+      std::cout << "MIS size: " << misSize << '\n';
     }
-    #endif
-
-    #if defined(KOKKOS_ENABLE_THREADS)
-    if(params.use_threads)
-    {
-      run_mis2<Kokkos::Threads>(params);
-      run = true;
-    }
-    #endif
-
-    #if defined(KOKKOS_ENABLE_CUDA)
-    if(params.use_cuda)
-    {
-      run_mis2<Kokkos::Cuda>(params);
-      run = true;
-    }
-    #endif
-
-    #if defined(KOKKOS_ENABLE_SERIAL)
-    if(params.use_serial)
-    {
-      run_mis2<Kokkos::Serial>(params);
-      run = true;
-    }
-    #endif
-
-    if(!run)
-    {
-      std::cerr << "*** ERROR: did not run, none of the supported device types were selected.\n";
-    }
-
     Kokkos::finalize();
-
     return 0;
 }
+
